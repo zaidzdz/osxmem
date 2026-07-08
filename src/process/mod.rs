ae8518cfd8410f1c398b9ffe86ccb8cc514f4e25
zaidzdz <81050752+zaidzdz@users.noreply.github.com>
2026-07-08 15:54:20 -0400

    added open by name for process

--- src/process/mod.rs
+++ src/process/mod.rs
@@ -1,8 +1,10 @@
-use std::iter::Product;
 use mach::port::{mach_port_t, MACH_PORT_NULL};
 use mach::kern_return::{kern_return_t, KERN_SUCCESS};
-use mach::mach_types::task_port_t;
 use mach::traps::{mach_task_self, task_for_pid};
+use libproc::{processes};
+use libproc::proc_pid::ProcType;
+use libproc::processes::ProcFilter;
+
 pub struct Process {
     pub pid: u32,
     pub port: mach_port_t,
@@ -11,6 +13,7 @@ pub struct Process {
 pub enum ProcessErr {
     PermissionDenied,        // KERN_FAILURE 5
     NoSuchProcess,           // KERN_INVALID_ARGUMENT 4
+    ListFailed(String),
     Other(kern_return_t),    // something else
 }
 impl ProcessErr {
@@ -25,12 +28,21 @@ impl ProcessErr {
 impl Process {
     pub fn open(pid: u32) -> Result<Process, ProcessErr> {
         let mut port:mach_port_t = MACH_PORT_NULL; //0
-        let kern_ret = unsafe {task_for_pid(mach_task_self(), pid as i32, &mut port)};
+        let kern_ret:kern_return_t = unsafe {task_for_pid(mach_task_self(), pid as i32, &mut port)};
 
         return if kern_ret != KERN_SUCCESS {
             Err(ProcessErr::from_kern(kern_ret))
         } else {
-            Ok(Process(pid, port))
+            Ok(Process{pid, port})
         }
+    }
+    pub fn open_by_name(name: &str) -> Result<Process, ProcessErr> {
+        let pids: Vec<u32> = processes::pids_by_type(ProcFilter::All).map_err(|e| ProcessErr::ListFailed(e.to_string()))?;
+
+        let pid = pids.iter()
+            .find(|pid| libproc::proc_pid::name(**pid as i32).ok().as_deref() == Some(name))
+            .ok_or(ProcessErr::NoSuchProcess)?;
+
+        Process::open(*pid)
     }
 }
